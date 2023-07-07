2ba926045a3a7e8e8897df3f9cbbd3894e534f2b
Shiran.Ahmed <Shiran.Ahmed@ril.com>
2023-07-08 00:51:35 +0530

    Nav Bar completed with one issue

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-
+import NavBar from "./scene/NavBar";
+import { useState } from "react";
+import useMediaQuery from "./hooks/useMediaQuery";
 
 function App() {
+  const [selectedPage, setSelectedPage] = useState("home");
+  const isAboveMediumScreen = useMediaQuery("(min-width: 1060px)");
   return (
-    <div className="App">
-     
+    <div className="app bg-deep-blue">
+      <NavBar selectedPage={selectedPage} setSelectedPage={selectedPage} />
     </div>
   );
 }
--- src/hooks/useMediaQuery.jsx
+++ src/hooks/useMediaQuery.jsx
@@ -10,8 +10,9 @@ const useMediaQuery = (query) => {
     }
     const listener = () => setMatches(media.matches);
     window.addEventListener("resize", listener);
-    return () => window.removeEventListener("resize, listener");
+    return () => window.removeEventListener("resize", listener);
   }, [matches, query]);
+
   return matches;
 };
 
