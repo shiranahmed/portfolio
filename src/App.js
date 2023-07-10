6aef30bb755b7a20d333651c031b0e3e5b0e5c16
Shiran.Ahmed <Shiran.Ahmed@ril.com>
2023-07-11 00:30:47 +0530

    added dotgroup components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import NavBar from "./scene/NavBar";
-import { useState } from "react";
+import DotGroup from "./scene/DotGroup.jsx";
+import { useEffect, useState } from "react";
 import useMediaQuery from "./hooks/useMediaQuery";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState("home");
   const isAboveMediumScreen = useMediaQuery("(min-width: 1060px)");
+  const [isTopOfPage, setIsTopOfPage] = useState(true);
+
+  useEffect(() => {
+    const handleScrol = () => {
+      if (window.scrollY === 0) setIsTopOfPage(true);
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleScrol);
+    return () => window.removeEventListener("scroll", handleScrol);
+  }, []);
+
   return (
     <div className="app bg-deep-blue">
-      <NavBar selectedPage={selectedPage} setSelectedPage={selectedPage} />
+      <NavBar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={selectedPage}
+      />
+      <div className="w-5/6 mx-aut0 md:h-full">
+        {isAboveMediumScreen && (
+          <DotGroup>
+            selectedPage={selectedPage}
+            setSelectedPage={selectedPage}
+          </DotGroup>
+        )}
+      </div>
     </div>
   );
 }
